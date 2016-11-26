7fa0db523dbc7619571edb53e4b30f9ecba91704
Nishith Khandwala <nishith@stanford.edu>
2016-11-26 02:37:54 -0800

    Implemented btc_to_usd_exchange_rate function using tanh

--- model_utils.py
+++ model_utils.py
@@ -1,17 +1,35 @@
 # Functions which Brandon will implement
 from __future__ import division
+import math
 
 blocks_per_period = 144
 asic_recuperation_period = 250
 block_reward = 12.5
 
 def btc_to_usd_exchange_rate(time, fraction_of_btc_stolen):
-    # time is absolute time
-    # TODO: Replace with something that makes sense
-    if fraction_of_btc_stolen > 0.0000001:
-        return 200
+    # Attack time, if applicable, is 0
+    # This function is modeled after tanh.
+    # After an attack the btc-to-usd rate drops dramatically.
+    # Assuming no further attacks, the rate slowly recovers.
+    # |   _ _ __
+    # |  /
+    # | /
+    # |/
+    # notice the free fall and then tanh(>=0)
+    default_btc_to_usd_rate = 730 # current bitcoin rate
+    if fraction_of_btc_stolen == 0.0:
+        return default_btc_to_usd_rate # assume rate is constant
     else:
-        return 500
+        # Crude assumption: rate decreases by fraction_of_btc_stolen %.
+        lower_bound_rate = (1.0 - fraction_of_btc_stolen) * default_btc_to_usd_rate
+        # Crude assumption: rate can __fully__ recover.
+        upper_bound_rate = 1.0 * default_btc_to_usd_rate
+        # Since attack time = 0, any time post attack is positive.
+        # And so, second derivative of rate is negative.
+        # This ranges from 0 to 1.
+        delta_rate = math.tanh(time) * (upper_bound_rate - lower_bound_rate)
+        new_btc_to_usd = lower_bound_rate + delta_rate
+        return new_btc_to_usd
 
 def mining_asic_sell_price(mining_power, fraction_of_btc_stolen, sell_machines_time,
                            discount_rate):
